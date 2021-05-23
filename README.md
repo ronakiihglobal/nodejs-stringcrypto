f99163b5bbcf33f72ebd9ba482710336f8fda18e
ronak.iihglobal <ronak.iihglobal@gmail.com>
2021-05-23 10:30:33 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## About StringCrypto
+
+This is simple package for encrypt/decrypt small strings like database IDs with ease. It is based on inbuilt node module `crypto`.
+
+The encrypt method used is `AES-256-CBC`.
+
+## Usage
+
+`npm i @ronak.iihglobal/stringcrypto`
+
+then 
+
+`const { encrypt, decrypt } = require('@ronak.iihglobal/stringcrypto')
+.init("random_secret_string_of_length_32","random_secret_string_of_length_16");` 
+
+Note init method and constraints on key and IV length.
+
+# or
+
+Just copy index.js file from github and put it in you project as it is simple one file module.
+
+Once module imported and init method called with proper arguments, we can simply use it like below,
+
+`let encrypted = encrypt("mystring")`
+
+`let decrypted = decrypt(encrypted)`
---
+++ index.js
@@ -0,0 +1,42 @@
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-cbc';
+let secretKey = "";
+let secretIV = "";
+
+
+
+const encrypt = (text) => {
+
+
+    if(!isNaN(text)){
+        text = text.toString()
+    }
+
+    const cipher = crypto.createCipheriv(algorithm, secretKey, secretIV);
+    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+    return encrypted.toString('hex');
+};
+
+const decrypt = (content) => {
+
+    const decipher = crypto.createDecipheriv(algorithm, secretKey, secretIV);
+    const decrpyted = Buffer.concat([decipher.update(Buffer.from(content, 'hex')), decipher.final()]);
+    return decrpyted.toString();
+};
+
+const init = (key, iv) => {
+
+    secretKey = key;
+    secretIV = iv;
+
+    return {
+        encrypt,
+        decrypt
+    }
+
+}
+
+module.exports = {
+    init
+};
